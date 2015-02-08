9724691e8be5b63e468aec0866e31431e9e7056d
lessthanproductive <davidrm@vt.edu>
2015-02-08 18:37:41 -0500

    New Textures and Animations
    Added new textures and added robot animations for unity

--- Assets/Scripts/CaptureNormals.cs
+++ Assets/Scripts/CaptureNormals.cs
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+//<<<<<<< Updated upstream
 using UnityEngine;
 using System.Collections;
 
@@ -45,51 +45,5 @@ public class CaptureNormals : MonoBehaviour
 
     }
 }
-=======
-using UnityEngine;
-using System.Collections;
 
-public class CaptureNormals : MonoBehaviour
-{
-    private Material mat;
-
-    void OnPostRender()
-    {
-        if(!mat)
-        {
-            mat = new Material("Shader \"Hidden/SetAlpha\" {" +
-            "SubShader {" +
-            "    Pass {" +
-            "        ZTest Always Cull Off ZWrite Off" +
-            "        SetTexture [_CameraNormalsTexture] { combine texture } " +
-            "   } " +
-            "   }" +
-            "}"
-            );
-        }
-
-
-        GL.PushMatrix();
-        GL.LoadOrtho();
-        for(int i = 0 ; i < mat.passCount ; ++i )
-        {
-            mat.SetPass(i);
-            GL.Begin(GL.QUADS);
-            GL.TexCoord(new Vector3(0, 0, 0));
-            GL.Vertex3(0, 0, 0.1f);
-            GL.TexCoord(new Vector3(1, 0, 0));
-            GL.Vertex3(1, 0, 0.1f);
-            GL.TexCoord(new Vector3(1, 1, 0));
-            GL.Vertex3(1, 1, 0.1f);
-            GL.TexCoord(new Vector3(0, 1, 0));
-            GL.Vertex3(0, 1, 0.1f);
-            GL.End();
-        }
-
-        GL.PopMatrix();
-
-
-
-    }
-}
->>>>>>> Stashed changes
+//>>>>>>> Stashed changes
